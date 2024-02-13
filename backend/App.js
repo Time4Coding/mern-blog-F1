263a69d6d194bf7ae5c648bebaa55a9dea75a40e
Hamza-Hasnni <baltidanger@live.fr>
2024-02-13 16:57:23 +0100

    fix bug

--- backend/App.js
+++ backend/App.js
@@ -1,31 +1,33 @@
 const express = require("express");
 const app = express();
-const dbConnection = require("./config/db")
+const dbConnection = require("./config/db");
 const port = process.env.PORT || 5000;
-const userRoute = require("./routes/user.route")
-const blogRoute = require("./routes/blog.route")
-const cookieParser = require('cookie-parser')
-const cors = require("cors")
+const userRoute = require("./routes/user.route");
+const blogRoute = require("./routes/blog.route");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-dbConnection()
+dbConnection();
 
 // node middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-// cokie parser middleware 
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// cokie parser middleware
+app.use(cookieParser());
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000",
     credentials: true,
-}))
-
+  })
+);
+app.set("trust proxy", 1);
 
 // All user Routes
-app.use("/api/user", userRoute)
+app.use("/api/user", userRoute);
 
 // All blog routes
-app.use("/api/blog", blogRoute)
+app.use("/api/blog", blogRoute);
 
 //Runing the Server to listen for request
-app.listen(port, () => console.log(`Server runing on port: ${port}`))
+app.listen(port, () => console.log(`Server runing on port: ${port}`));
